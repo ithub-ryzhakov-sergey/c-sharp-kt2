774363c90815cc9fda6ef44b983ebd99cf95025b
user1 <user1@DESKTOP-ENRJJP4>
2025-09-13 16:53:43 +0300

    решение задания 3

--- App/Topics/Indexers/T1_3_Matrix2D/Matrix2D.cs
+++ App/Topics/Indexers/T1_3_Matrix2D/Matrix2D.cs
@@ -7,6 +7,9 @@
 // - Индексатор get/set с проверкой границ (row in [0, Rows), col in [0, Cols)), иначе ArgumentOutOfRangeException.
 // - Отображение (row, col) -> index: row * Cols + col.
 
+using App.Topics.Indexers.T1_1_IntList;
+using System.Data;
+
 namespace App.Topics.Indexers.T1_3_Matrix2D;
 
 public class Matrix2D
@@ -16,4 +19,50 @@ public class Matrix2D
     // 2) Реализуйте конструктор Matrix2D(int rows, int cols) с проверкой > 0.
     // 3) Реализуйте свойства Rows, Cols.
     // 4) Реализуйте индексатор this[int row, int col] c проверкой границ и отображением к одномерному индексу.
+
+    private int[] arr;
+    private int rows;
+    private int cols;
+
+    public Matrix2D(int rows, int cols)
+    {
+        if (rows <= 0)
+            throw new ArgumentOutOfRangeException($"{nameof(rows)} < 0");
+        if (cols <= 0)
+            throw new ArgumentOutOfRangeException($"{nameof(cols)} < 0");
+
+        this.rows = rows;
+        this.cols = cols;
+        this.arr = new int[cols * rows];
+    }
+
+    public int this[int row, int col]
+    {
+        get
+        {
+            Validate(row, col);
+            var index = GetIndex(row, col);
+            return this.arr[index];
+        }
+
+        set
+        {
+            Validate(row, col);
+            var index = GetIndex(row, col);
+            this.arr[index] = value;
+        }
+    }
+
+    private void Validate(int row, int col)
+    {
+        if ((row >= 0 && row < this.rows) is false)
+            throw new ArgumentOutOfRangeException($"{nameof(row)} не входит {this.rows - 1}");
+        if ((col >= 0 && col < this.cols) is false)
+            throw new ArgumentOutOfRangeException($"{nameof(col)} не входит {this.cols - 1}");
+    }
+
+    private int GetIndex(int row, int col)
+    {
+        return row * this.cols + col;
+    }
 }
